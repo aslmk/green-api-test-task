463ad0d000186c7f725c9df659010330391dd01e
aslmk <aulaensov@gmail.com>
2026-09-25 07:00:51 +0500

    Initial commit

---
+++ src/main/java/com/aslmk/greenapitest/GreenApiTestApplication.java
@@ -0,0 +1,13 @@
+package com.aslmk.greenapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenApiTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenApiTestApplication.class, args);
+    }
+
+}
